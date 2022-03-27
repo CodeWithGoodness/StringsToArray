92c3510cf7889f6efce02cabe52429cca802fd3e
CodeWithGoodness <ceciliaOkoro2@gmail.com>
2022-03-27 12:26:51 -0700

    first commit

---
+++ src/Strings_array.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Strings_array {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+     String name =   "Goodness";
+        char[] my_name = name.toCharArray();
+        System.out.println (my_name [0]);
+    }
+}
